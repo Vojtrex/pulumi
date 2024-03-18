0059a75047657179ea2f2cd13b47c0a3498b3a66
Vojtech Navratil <navryvoj@gmail.com>
2024-03-18 14:59:21 +0100

    aurora serverless RDS configured

--- components/rds.py
+++ components/rds.py
@@ -12,27 +12,31 @@ sys.path.append(parent_dir)
 
 import vars
 
-# Create the Aurora Serverless v2 cluster 
+# Create the Aurora Serverless v2 cluster
 aurora_cluster = aws.rds.Cluster("aurora_cluster",
                                  cluster_identifier=vars.cluster_name,
                                  engine="aurora-postgresql",
                                  engine_mode='provisioned',
-                                 engine_version="13.6",
+                                 engine_version="16.1",
                                  database_name=vars.db_name,
                                  master_username=vars.db_master_username,
                                  master_password=vars.db_master_password,
                                  storage_encrypted=True,
                                  vpc_security_group_ids=[vpc.security_group_db.id],
+                                 skip_final_snapshot=True,
                                  serverlessv2_scaling_configuration=aws.rds.ClusterServerlessv2ScalingConfigurationArgs(
                                      min_capacity=0.5,
                                      max_capacity=1,
                                  ))
 
+
+# Create a Aurora Cluster Instance
 aurora_cluster_instance = aws.rds.ClusterInstance("aurora_cluster_instance",
                                                   cluster_identifier=aurora_cluster.id,
                                                   instance_class="db.serverless",
                                                   engine=aurora_cluster.engine,
-                                                  engine_version=aurora_cluster.engine_version
+                                                  engine_version=aurora_cluster.engine_version,
+                                                  publicly_accessible=True,
                                                   )
 
 # Export the cluster endpoint to allow connections to the database
