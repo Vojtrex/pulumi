347796ff48eae60f163bb3f1e0387cde6b1adb8a
Vojtech Navratil <navryvoj@gmail.com>
2024-03-21 16:31:58 +0100

    allow https

--- components/vpc.py
+++ components/vpc.py
@@ -13,7 +13,6 @@ import vars
 # Read back the default VPC and public subnets, which we will use.
 default_vpc = aws.ec2.get_vpc(default=True)
 
-
 # Get all subnets in the default VPC
 subnets = aws.ec2.get_subnets(filters=[{"name": "vpc-id", "values": [default_vpc.id]}])
 
@@ -32,22 +31,28 @@ pulumi.export('default_subnet_2_id', default_subnet_2.id)
 pulumi.export('default_subnet_3_id', default_subnet_3.id)
 
 # Create a SecurityGroup that permits HTTP ingress and unrestricted egress.
-group = aws.ec2.SecurityGroup('web-secgrp',
-                              vpc_id=default_vpc.id,
-                              description='Enable HTTP access',
-                              ingress=[aws.ec2.SecurityGroupIngressArgs(
-                                  protocol='tcp',
-                                  from_port=80,
-                                  to_port=80,
-                                  cidr_blocks=['0.0.0.0/0'],
-                              )],
-                              egress=[aws.ec2.SecurityGroupEgressArgs(
-                                  protocol='-1',
-                                  from_port=0,
-                                  to_port=0,
-                                  cidr_blocks=['0.0.0.0/0'],
-                              )],
-                              )
+security_group_allow_all_traffic = aws.ec2.SecurityGroup('vpc_security_group_allow_all_traffic',
+                                                         vpc_id=default_vpc.id,
+                                                         description='Enable HTTP and HTTPS access',
+                                                         ingress=[aws.ec2.SecurityGroupIngressArgs(
+                                                             protocol='tcp',
+                                                             from_port=80,
+                                                             to_port=80,
+                                                             cidr_blocks=['0.0.0.0/0'],
+                                                         ),
+                                                             aws.ec2.SecurityGroupIngressArgs(
+                                                                 protocol='tcp',
+                                                                 from_port=443,
+                                                                 to_port=443,
+                                                                 cidr_blocks=['0.0.0.0/0'],
+                                                             )],
+                                                         egress=[aws.ec2.SecurityGroupEgressArgs(
+                                                             protocol='-1',
+                                                             from_port=0,
+                                                             to_port=0,
+                                                             cidr_blocks=['0.0.0.0/0'],
+                                                         )],
+                                                         )
 
 # Create a security group to control access to the database
 security_group_db = aws.ec2.SecurityGroup("aurora_security_group",
